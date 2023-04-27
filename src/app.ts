9c3212dadd5cf5234739bd884be84fb86c24a10c
Aseem PM <aseem@phaethon.tech>
2023-04-27 13:39:59 +0530

    changed the path

--- src/app.ts
+++ src/app.ts
@@ -12,19 +12,28 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send(data);
-});
+app.get(
+  "/.netlify/functions/app",
+  (req: Request, res: Response, next: NextFunction) => {
+    res.send(data);
+  }
+);
 
-app.get("/:loadId", (req: Request, res: Response, next: NextFunction) => {
-  res.send(data.find((data) => data.loanId == req.params.loadId));
-});
+app.get(
+  "/.netlify/functions/app/:loadId",
+  (req: Request, res: Response, next: NextFunction) => {
+    res.send(data.find((data) => data.loanId == req.params.loadId));
+  }
+);
 
-app.post("/", (req: Request, res: Response, next: NextFunction) => {
-  const loanExist = data.some((data) => data.loanId == req.body.loanId);
-  if (!loanExist) data.push(req.body);
-  res.send(req.body);
-});
+app.post(
+  "/.netlify/functions/app",
+  (req: Request, res: Response, next: NextFunction) => {
+    const loanExist = data.some((data) => data.loanId == req.body.loanId);
+    if (!loanExist) data.push(req.body);
+    res.send(req.body);
+  }
+);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
